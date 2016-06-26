d74d7cadd0a38cab0096a375d49de6edb2989e63
ElderAbyss <mail.jody.c@gmail.com>
2016-06-25 23:13:01 -0400

    Changed some columns to allow NULL in the DB Script

--- dbscripts/AlienAbduct.sql
+++ dbscripts/AlienAbduct.sql
@@ -22,11 +22,11 @@ USE `Alien_Abductors` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Alien_Abductors`.`Location` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `State` VARCHAR(45) NOT NULL,
-  `Country` VARCHAR(45) NOT NULL,
-  `City` VARCHAR(45) NOT NULL,
-  `Zipcode` TINYINT(15) NOT NULL,
-  `Address` VARCHAR(255) NOT NULL,
+  `State` VARCHAR(45) NULL,
+  `Country` VARCHAR(45) NULL,
+  `City` VARCHAR(45) NULL,
+  `Zipcode` TINYINT(15) NULL,
+  `Address` VARCHAR(255) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -47,13 +47,13 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Alien_Abductors`.`Members` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `First_Name` VARCHAR(45) NOT NULL,
-  `Last_Name` VARCHAR(45) NOT NULL,
-  `Age` TINYINT NOT NULL,
-  `location_id` INT UNSIGNED NOT NULL,
-  `User_id` INT UNSIGNED NOT NULL,
+  `First_Name` VARCHAR(45) NULL,
+  `Last_Name` VARCHAR(45) NULL,
+  `Age` TINYINT NULL,
+  `location_id` INT UNSIGNED NULL,
+  `User_id` INT UNSIGNED NULL,
   `Username` VARCHAR(45) NOT NULL,
-  `Email` VARCHAR(255) NOT NULL,
+  `Email` VARCHAR(255) NULL,
   `Password` VARCHAR(45) NOT NULL,
   `Privilege` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS `Alien_Abductors`.`Members` (
 CREATE TABLE IF NOT EXISTS `Alien_Abductors`.`Experience` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `Exp_Name` VARCHAR(45) NOT NULL,
-  `Description` TEXT NOT NULL,
+  `Description` TEXT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -87,11 +87,11 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Alien_Abductors`.`Exp_Reviews` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `Review` TEXT(1000) NOT NULL,
-  `Exp_Rating` TINYINT(5) NOT NULL,
+  `Review` TEXT(1000) NULL,
+  `Exp_Rating` TINYINT(5) NULL,
   `Member_id` INT UNSIGNED NOT NULL,
   `Exp_id` INT UNSIGNED NOT NULL,
-  `Recommend` TINYINT(1) NOT NULL,
+  `Recommend` TINYINT(1) NULL,
   PRIMARY KEY (`id`),
   INDEX `Member_id_idx` (`Member_id` ASC),
   INDEX `Exp_id_idx` (`Exp_id` ASC),
@@ -111,9 +111,3 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
---Aline Aductors App User
-CREATE USER 'alienapp'@'localhost' IDENTIFIED BY '9DG74*FMc5kQBK8LwVUM';
-# Privileges for `alienapp`@`%`
-GRANT USAGE ON *.* TO 'alienapp'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `alien_abductors`.* TO 'alienapp'@'localhost';
